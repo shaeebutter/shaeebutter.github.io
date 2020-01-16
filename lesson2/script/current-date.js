78843f5769f0f64e3e43585ab46589ff002321d5
shaeebutter <whi16032@byui.edu>
2020-01-16 12:01:55 -0700

    changes 1-16 12pm

---
+++ lesson2/script/current-date.js
@@ -0,0 +1,10 @@
+    document.getElementById('year').innerText = new Date().getFullYear()
+
+   /*or you can use these two 
+    var currentYear = currentDate.getFullYear();
+    onsole.log(currentYear);
+
+    const currentDate = new Date(); 
+    console.log(currentDate)
+    */
+   
