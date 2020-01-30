d8ff949196974878689dd1193bb219bf85216291
shaeebutter <whi16032@byui.edu>
2020-01-30 11:02:40 -0700

    added date at bottom

---
+++ lesson4/script/main.js
@@ -0,0 +1,37 @@
+/* Date format: Wed, 20 May 2020 */
+
+let currentDate = new Date();
+
+let year = currentDate.getFullYear()
+let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August', 
+    'September',
+    'October',
+    'November',
+    'December'
+]
+let  daysofweek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+]
+let dayofweek = currentDate.getDay()
+let numberday = currentDate.getDate()
+
+let fullDate = daysofweek[dayofweek];
+fullDate += ", " + numberday + " " + months[currentDate.getMonth()] + " " + year;
+
+document.querySelector('#current-date').textContent = fullDate;
+
+
