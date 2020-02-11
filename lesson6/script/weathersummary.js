b78657aaba09df6448a73260817608c54390520e
shaeebutter <whi16032@byui.edu>
2020-02-11 10:16:34 -0700

    changes

---
+++ lesson6/script/weathersummary.js
@@ -0,0 +1,3 @@
+document.querySelector('#currenttemp').innerHTML = '50&deg;';
+document.querySelector('#humidity').innerHTML = '10mph';
+document.querySelector('#windspeed').innerHTML = '10&percnt;';
