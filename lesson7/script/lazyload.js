38e2514c1f79f27e91113cdeb0787d2a506cf750
shaeebutter <whi16032@byui.edu>
2020-02-22 22:49:42 -0700

    lazy load working now

--- lesson7/script/lazyload.js
+++ lesson7/script/lazyload.js
@@ -1,13 +1,11 @@
 const imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
+    console.log(image.getAttribute('data-src'))
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {
         image.removeAttribute('data-src');
     };
 };
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
 if ('IntersectionObserver' in window) {
     console.log('if')
     const observer = new IntersectionObserver((items, observer) => {
