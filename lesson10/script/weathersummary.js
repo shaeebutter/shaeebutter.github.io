3620c92e33b17c7e27616951043271443d7b64ac
shaeebutter <whi16032@byui.edu>
2020-03-12 09:52:39 -0600

    current temp

--- lesson10/script/weathersummary.js
+++ lesson10/script/weathersummary.js
@@ -1,18 +1,17 @@
-// const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=3bc8d0eed950aefc26f97b8d2a9e475e&units=imperial'
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=3bc8d0eed950aefc26f97b8d2a9e475e&units=imperial'
 
-// fetch(apiURL) 
-//     .then(
-//         response => (
-//             response.json()
-//             )
-//     .then (
-//         jsObject =>{
-//             document.getElementById('current-temp').textContent = jsObject.main.temp;
-//             let image = 'https://openweathermap.org/img/wn/' + jsObject.weather[0].icon + '.png';
-//             document.getElementById('weather-icon').setAttribute('src', image);
+fetch(apiURL) 
+    .then(
+        response => (
+            response.json()
+            )
+    .then (
+        jsObject =>{
+            document.getElementById('current-temp').textContent = jsObject.main.temp;
+            let image = 'https://openweathermap.org/img/wn/' + jsObject.weather[0].icon + '.png';
+            document.getElementById('weather-icon').setAttribute('src', image);
         
-//             let alt= jsObject.weather[0].description;
-//             document.getElementById('weather-icon').setAttribute('alt', alt);
+            let alt= jsObject.weather[0].description;
+            document.getElementById('weather-icon').setAttribute('alt', alt);
 
-//         }
-//     ));
+         }));
